c39c2e754e8155a09ccd459d4763eeb1f58cccfc
Timothy Felt <timothyfelt@MacBook-Pro-4.local>
2025-02-05 17:10:31 -0700

    Part 2 in Depth HAL myFUnctions complete

--- Src/hal_gpio.c
+++ Src/hal_gpio.c
@@ -1,33 +1,66 @@
 #include <stdint.h>
 #include <stm32f0xx_hal.h>
 #include <stm32f0xx_hal_gpio.h>
-/*
+
 void My_HAL_GPIO_Init(GPIO_TypeDef  *GPIOx, GPIO_InitTypeDef *GPIO_Init)
 {
-}
-*/
+    if (GPIOx == GPIOA)
+    {
+        GPIOx->MODER &=~(GPIO_MODER_MODER0);
+        GPIOx->OSPEEDR &=~(GPIO_OSPEEDR_OSPEEDR0);
+        GPIOx->PUPDR |=(GPIO_PUPDR_PUPDR0_1);
+    }
+    if (GPIOx == GPIOC)
+    {
+        GPIOx->MODER |=(GPIO_MODER_MODER7_0 | GPIO_MODER_MODER6_0|GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0);
+        // Register OTYPER is in the correct state.
+        //Register Ospeedr is already in the correct state.
+        // PUPDR Is already in the correct state.
+    }
 
-/*
+}
+
+void My_HAL_RCC_GPIOC_CLK_ENABLE(){
+    RCC->AHBENR |=(RCC_AHBENR_GPIOCEN);
+}
+
 void My_HAL_GPIO_DeInit(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin)
 {
 }
-*/
 
-/*
+
+
 GPIO_PinState My_HAL_GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 {
-    return -1;
+    uint16_t ReadBit = GPIOx->IDR & GPIO_Pin;
+    if (ReadBit == 0){
+        return 0;
+    }
+    else {
+        return 1;
+    }
 }
-*/
 
-/*
+
+
 void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
 {
+    if(PinState == GPIO_PIN_SET){
+        GPIOx->BSRR = GPIO_Pin;
+    }
+    else {
+        GPIOx->BRR = GPIO_Pin;
+    }
 }
-*/
 
-/*
+
+
 void My_HAL_GPIO_TogglePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 {
+    uint16_t temp = GPIOx->ODR;
+    GPIOx->BRR = GPIO_Pin;
+    GPIOx->BSRR = temp ^ GPIO_Pin;
+
+
 }
-*/
+
--- Src/lab1.c
+++ Src/lab1.c
@@ -1,18 +1,23 @@
 #include "main.h"
 #include <stm32f0xx_hal.h>
+#include <hal_gpio.h>
 
 int lab1_main(void){
     HAL_Init();
     SystemClock_Config();
 
-    __HAL_RCC_GPIOC_CLK_ENABLE();
+    My_HAL_RCC_GPIOC_CLK_ENABLE();
 
     GPIO_InitTypeDef initStr = {GPIO_PIN_7 | GPIO_PIN_6, GPIO_MODE_OUTPUT_PP, GPIO_SPEED_FREQ_LOW, GPIO_NOPULL};
 
-    HAL_GPIO_Init(GPIOC, &initStr);
-    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
+    GPIO_InitTypeDef initButton = {GPIO_PIN_0, GPIO_MODE_INPUT, GPIO_SPEED_FREQ_LOW, GPIO_PULLDOWN};
+
+    My_HAL_GPIO_Init(GPIOA, &initButton);
+
+    My_HAL_GPIO_Init(GPIOC, &initStr);
+    My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
     while (1) {
         HAL_Delay(200);
-        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7 | GPIO_PIN_6);
+        My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7 | GPIO_PIN_6);
     }      
 }
