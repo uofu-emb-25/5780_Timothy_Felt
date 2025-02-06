38c1f8738cc2a22b45bf25eca32beaf51b2b481c
Timothy Felt <timothyfelt@MacBook-Pro-4.local>
2025-02-05 17:23:39 -0700

    Checkoff 3 Finished.

--- Src/lab1.c
+++ Src/lab1.c
@@ -7,6 +7,7 @@ int lab1_main(void){
     SystemClock_Config();
 
     My_HAL_RCC_GPIOC_CLK_ENABLE();
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 
     GPIO_InitTypeDef initStr = {GPIO_PIN_7 | GPIO_PIN_6, GPIO_MODE_OUTPUT_PP, GPIO_SPEED_FREQ_LOW, GPIO_NOPULL};
 
@@ -16,8 +17,26 @@ int lab1_main(void){
 
     My_HAL_GPIO_Init(GPIOC, &initStr);
     My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
-    while (1) {
+    /*while (1) {
         HAL_Delay(200);
         My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7 | GPIO_PIN_6);
-    }      
+    } */
+    uint32_t debouncer = 0;
+    while(1) {
+    debouncer = (debouncer << 1); // Always shift every loop iteration
+    if (My_HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0)) { // If input signal is set/high
+    debouncer |= 0x01; // Set lowest bit of bit-vector
+    }
+    if (debouncer == 0xFFFFFFFF) {
+    // This code triggers repeatedly when button is steady high!
+    }
+    if (debouncer == 0x00000000) {
+    // This code triggers repeatedly when button is steady low!
+    }
+    if (debouncer == 0x7FFFFFFF) {
+        My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6 | GPIO_PIN_7);
+    // This code triggers only once when transitioning to steady high!
+    }
+    // When button is bouncing the bit-vector value is random since bits are set when the button is high and not when it bounces low.
+    }
 }
