0d778e639fcc4416196b5e3ad4f3304e2d503f3a
Timothy Felt <timothyfelt@MacBook-Pro-5.local>
2025-02-12 07:59:48 -0700

    Moved the HAL header files over from main.h for consistency sake.

--- Inc/hal_gpio.h
+++ Inc/hal_gpio.h
@@ -9,3 +9,9 @@ void EXTI0_1_IRQHandler(void);
 GPIO_PinState My_HAL_GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
 void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState);
 void My_HAL_GPIO_TogglePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+void My_HAL_TIMED_TogglePin();
+void My_HAL_RCC_GPIOC_CLK_ENABLE(void);
+void SYSCONFIG_ROUTE_PA0_EXTI0(void);
+void EXTI_INTERRUPT(void);
+void SystemClock_Config(void);
+
--- Inc/main.h
+++ Inc/main.h
@@ -1,10 +1,6 @@
 #pragma once
 #include <stdint.h>
 
-void SystemClock_Config(void);
-void My_HAL_RCC_GPIOC_CLK_ENABLE(void);
-void SYSCONFIG_ROUTE_PA0_EXTI0(void);
-void EXTI_INTERRUPT(void);
 int lab1_main(void);
 int lab2_main(void);
 int lab3_main(void);
